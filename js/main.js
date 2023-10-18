596a1f2d0072c1ed32855904829a555953a9146b
YukaTomita <132251142+YukaTomita@users.noreply.github.com>
2023-10-18 13:36:28 +0900

    JS
    java script

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// ヘッダーを読み込み、挿入する
+fetch('/mist_recruit/include/header.html')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('ヘッダーファイルを読み込めませんでした。');
+    }
+    return response.text();
+  })
+  .then(headerHTML => {
+    const header = document.querySelector('#header');
+    header.insertAdjacentHTML('afterbegin', headerHTML);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// フッターを読み込み、挿入する
+fetch('/mist_recruit/include/footer.html')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('フッターファイルを読み込めませんでした。');
+    }
+    return response.text();
+  })
+  .then(footerHTML => {
+    const footer = document.querySelector('#footer');
+    footer.insertAdjacentHTML('beforeend', footerHTML);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// メニューのクリックイベントを処理する（jQueryを使用）
+$(document).on('click', '.burger-btn', function () {
+  $('.bar').toggleClass('cross');
+  $('.header-nav').toggleClass('open');
+  $('.burger-musk').fadeToggle(300);
+  $('body').toggleClass('noscroll');
+});
